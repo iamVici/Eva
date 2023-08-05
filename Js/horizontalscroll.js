8b1107e12f7a9b19678cc8eeff355d10593f1253
Vici <victorpires.camposs@gmail.com>
2023-08-05 15:45:11 -0300

    section 2 base

--- Js/horizontalscroll.js
+++ Js/horizontalscroll.js
@@ -1,14 +1,23 @@
 var isScrolling = false;
+var isMouseOverTimeline = false;  // Nova linha
 var currentSectionIndex = 0;
 var sections = document.querySelectorAll('.section');
 var wrapper = document.querySelector('#wrapper');
 var sectionPositions = Array.from(sections).map(sec => sec.offsetLeft);
 var menuLinks = document.querySelectorAll('.menu .section-link');
 
-function handleScroll(event) {
-    if (isScrolling) return;
-    isScrolling = true;
+// Novas linhas
+var timelineDiv = document.querySelector('.mytimeline');
+timelineDiv.addEventListener('mouseover', function() {
+  isMouseOverTimeline = true;
+});
+timelineDiv.addEventListener('mouseout', function() {
+  isMouseOverTimeline = false;
+});
 
+function handleScroll(event) {
+    if (isScrolling || isMouseOverTimeline) return; // Adicionamos verificação isMouseOverTimeline aqui
+    isScrolling = true;
     event.preventDefault();
     var nextSectionIndex = event.deltaY > 0 
         ? Math.min(currentSectionIndex + 1, sections.length - 1)
@@ -25,24 +34,15 @@ function handleScroll(event) {
         }
     });
     
-    // Remove a classe "active" de todos os links do menu
     menuLinks.forEach(a => a.classList.remove('active'));
-    
-    // Adicione a classe "active" ao link do menu correspondente à seção ativa
     menuLinks[nextSectionIndex].classList.add('active');
-    
     currentSectionIndex = nextSectionIndex;
 }
 
-// Adicione um ouvinte de eventos de clique a cada link do menu
 menuLinks.forEach(a => {
     a.addEventListener('click', function(event) {
         event.preventDefault();
-
-        // Obtenha o índice da seção correspondente ao item do menu clicado
         var sectionIndex = parseInt(this.getAttribute('data-order')) - 1;
-
-        // Role para a seção correspondente
         gsap.to(wrapper, {
             scrollTo: {
                 x: sectionPositions[sectionIndex],
@@ -53,20 +53,11 @@ menuLinks.forEach(a => {
                 isScrolling = false;
             }
         });
-
-        // Remova a classe "active" de todos os links do menu
         menuLinks.forEach(a => a.classList.remove('active'));
-
-        // Adicione a classe "active" ao link do menu clicado
         this.classList.add('active');
-
         currentSectionIndex = sectionIndex;
     });
 });
 
-// Adicione a classe "active" ao primeiro link do menu
 menuLinks[0].classList.add('active');
-
-// Adicione um ouvinte de eventos de rolagem ao documento
 document.addEventListener('wheel', handleScroll, { passive: false });
-
